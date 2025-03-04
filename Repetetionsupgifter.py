5677190be255e09db94590b3cabebee539602d35
Mehmet-Erkmen <mehmet.erkmen@elev.ga.ntig.se>
2025-03-04 10:35:21 +0100

    Add files via upload
    Första versionen av repetionsuppgift

---
+++ Repetetionsupgifter.py
@@ -0,0 +1,65 @@
+#1
+
+# def kontrollera_tal(x):
+#     if x % 2  == 0 :
+#         print("Talet", x , "är jämn")
+#     else:
+#         print("Talet", x , "är udda")
+
+
+# tal = int(input("Ge ett nummer? "))
+
+# kontrollera_tal(tal)'
+
+#2
+
+# text = "Python är roligt"
+
+# print(text.upper())
+# print(text.lower())
+# print(text.capitalize())
+
+#3
+
+# djur = input("Vad är ditt favoritdjur? ")
+
+# print(f"Din favorit djur är {djur}")
+
+#4
+
+# min_text = ""
+# print(len(min_text))
+
+#5
+
+# nummer = [5,15,25,35,45]
+# print(sum(nummer))
+
+#6 
+
+# fruit = ["Orange", "Peach", "Apple", "Greengage"]
+
+# fruit.remove("Orange")
+# print(fruit)
+# for i in fruit:
+#     print(i)
+
+
+# def räkna_e():
+#     text = "Hej, trevligt att träffas!"
+#     print(text.count("e"))
+
+# räkna_e()
+
+# nummer_lista = [10,20,30,40,50,60,70,80]
+# print(nummer_lista[0:3:1])
+# print(nummer_lista[1:6:2])
+# print(nummer_lista[-4:-1:1])
+
+#8
+
+min_meny = ["Pizza", "Hamburgare", "Sallad", "Soppa", "Pasta"]
+
+print(min_meny[::3])
+print(min_meny[::-1])
+
